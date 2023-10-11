450b21840085e05cbbd7c58e3c08fbd2a59e7cac
Legata <legata93@gmail.com>
2023-10-11 21:56:37 +0300

    Version 1.0

---
+++ src/js/__tests__/test.js
@@ -0,0 +1,16 @@
+import sortHeroes from '../app';
+
+test('testing right order in sorted function', () => {
+  const characters = [
+    { name: 'мечник', health: 10 },
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+  ];
+  const result = sortHeroes(characters);
+  const expected = [
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+    { name: 'мечник', health: 10 },
+  ];
+  expect(result).toEqual(expected);
+});
